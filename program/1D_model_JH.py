ccb3d2291a1221341634aa40bf212791d2a715ac
j-haralds <jonatan.haraldsson@icloud.com>
2026-01-29 12:16:03 +0100

    sünk

--- program/1D_model_JH.py
+++ program/1D_model_JH.py
@@ -3,33 +3,46 @@
 # run it: python3 1D_model_JH.py
 
 
-import matplotlib.pyplot as plt
-import pybamm 
-import numpy as np
+# import matplotlib.pyplot as plt
+# import pybamm 
+# import numpy as np
 
-model = pybamm.lithium_ion.DFN()
-print(model)
+# model = pybamm.lithium_ion.DFN()
+# print(model)
 
-geometry = model.default_geometry
-
-
-
-param = model.default_parameter_values
-
-param.process_model(model)
-param.process_geometry(geometry)
-
-mesh = pybamm.Mesh(geometry, model.default_submesh_types, model.default_var_pts)
-
-disc = pybamm.Discretisation(mesh, model.default_spatial_methods)
-disc.process_model(model)
+# geometry = model.default_geometry
 
 
-solver = model.default_solver
-t_eval = np.linspace(0, 3600, 100)
-solution = solver.solve(model, t_eval)
 
-plot = pybamm.QuickPlot(solution)
+# param = model.default_parameter_values
 
-plot.dynamic_plot()
+# param.process_model(model)
+# param.process_geometry(geometry)
 
+# mesh = pybamm.Mesh(geometry, model.default_submesh_types, model.default_var_pts)
+
+# disc = pybamm.Discretisation(mesh, model.default_spatial_methods)
+# disc.process_model(model)
+
+
+# solver = model.default_solver
+# t_eval = np.linspace(0, 3600, 100)
+# solution = solver.solve(model, t_eval)
+
+# plot = pybamm.QuickPlot(solution)
+
+# plot.dynamic_plot()
+
+
+
+import pybamm
+
+# load model
+model = pybamm.lithium_ion.SPMe()
+
+# create simulation
+simulation = pybamm.Simulation(model)
+
+simulation.solve([0, 3600])  # time interval in seconds
+
+simulation.plot()
