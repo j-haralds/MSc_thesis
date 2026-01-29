33f647d4c6d4ab28afe570c6c67859771c3c9843
j-haralds <jonatan.haraldsson@icloud.com>
2026-01-29 13:24:05 +0100

    tester

--- program/1D_model_JH.py
+++ program/1D_model_JH.py
@@ -5,49 +5,51 @@
 # source .venv/bin/activate
 
 
-# import matplotlib.pyplot as plt
-# import pybamm 
-# import numpy as np
+import matplotlib.pyplot as plt
+import pybamm 
+import numpy as np
 
-# model = pybamm.lithium_ion.DFN()
-# print(model)
+model = pybamm.lithium_ion.DFN()
+print(model)
 
-# geometry = model.default_geometry
-
-
-
-# param = model.default_parameter_values
-
-# param.process_model(model)
-# param.process_geometry(geometry)
-
-# mesh = pybamm.Mesh(geometry, model.default_submesh_types, model.default_var_pts)
-
-# disc = pybamm.Discretisation(mesh, model.default_spatial_methods)
-# disc.process_model(model)
-
-
-# solver = model.default_solver
-# t_eval = np.linspace(0, 3600, 100)
-# solution = solver.solve(model, t_eval)
-
-# plot = pybamm.QuickPlot(solution)
-
-# plot.dynamic_plot()
+geometry = model.default_geometry
 
 
 
-import pybamm
-import plot_settings
-plot_settings.apply()
-# load model
-model = pybamm.lithium_ion.SPMe()
+param = model.default_parameter_values
 
-# create simulation
-simulation = pybamm.Simulation(model)
+param.process_model(model)
+param.process_geometry(geometry)
 
-simulation.solve([0, 3600])  # time interval in seconds
+mesh = pybamm.Mesh(geometry, model.default_submesh_types, model.default_var_pts)
 
-simulation.plot()
+disc = pybamm.Discretisation(mesh, model.default_spatial_methods)
+disc.process_model(model)
+
+
+solver = model.default_solver
+t_eval = np.linspace(0, 3600, 100)
+solution = solver.solve(model, t_eval)
+
+plot = pybamm.QuickPlot(solution)
+
+
+print(model.default_parameter_values)
+plot.dynamic_plot()
+
+
+
+# import pybamm
+# import plot_settings
+# plot_settings.apply()
+# # load model
+# model = pybamm.lithium_ion.SPMe()
+
+# # create simulation
+# simulation = pybamm.Simulation(model)
+
+# simulation.solve([0, 3600])  # time interval in seconds
+
+# simulation.plot()
 
 
