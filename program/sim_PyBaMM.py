697569434130121e816d72f7e563be52d259289c
j-haralds <jonatan.haraldsson@icloud.com>
2026-02-10 17:38:18 +0100

    adieu

--- program/sim_PyBaMM.py
+++ program/sim_PyBaMM.py
@@ -9,8 +9,7 @@ def simulate(I0):
         return I0
     
     param["Current function [A]"] = my_current
-    param["Nominal cell capacity [A.h]"] = 10.
-    param['Number of cells connected in series to make a battery'] = 1.
+    param["Nominal cell capacity [A.h]"] = 2.
 
 
     # param = pybamm.ParameterValues(
@@ -26,9 +25,9 @@ def simulate(I0):
     mesh = pybamm.Mesh(geometry, model.default_submesh_types, model.default_var_pts)
     disc = pybamm.Discretisation(mesh, model.default_spatial_methods)
     disc.process_model(model)
-    t_eval = [0,3*3600]
+    t_eval = np.linspace(0,3600 / I0,100)
 
-    solver = pybamm.IDAKLUSolver(atol=1e-6, rtol=1e-3)
+    solver = pybamm.IDAKLUSolver(atol=1e-7, rtol=1e-5)
     solution = solver.solve(model, t_eval)
 
     return solution,model
@@ -37,25 +36,26 @@ def simulate(I0):
 def get_voltage(I):
     solution, model = simulate(I)
     npc = solution.observe(model.variables['Battery voltage [V]'])
-    t_ = np.linspace(0,solution['Time [s]'].entries[-1],101)
+    t_ = np.linspace(0,solution['Time [s]'].entries[-1],100)
     return t_,npc(t_)
 
 
 def get_OC_voltage(I):
     solution, model = simulate(I)
     npc = solution.observe(model.variables['Battery open-circuit voltage [V]'])
-    t_ = np.linspace(0,solution['Time [s]'].entries[-1],101)
+    t_ = np.linspace(0,solution['Time [s]'].entries[-1],1000)
     return t_,npc(t_)
 
 
 def get_discharge_capacity(I):
     solution, model = simulate(I)
     npc = solution.observe(model.variables['Discharge capacity [A.h]'])
-    t_ = np.linspace(0,solution['Time [s]'].entries[-1],101)
+    print(len(solution['Time [s]'].entries))
+    t_ = np.linspace(0,solution['Time [s]'].entries[-1],1000)
     return t_,npc(t_)
 
-def get_discharge_capacity(I):
+def get_discharge_capacity_II(I):
     solution, model = simulate(I)
     npc = solution.observe(model.variables['Discharge capacity [A.h]'])
-    t_ = np.linspace(0,solution['Time [s]'].entries[-1],101)
-    return t_,npc(t_)
+    t_ = np.linspace(0,solution['Time [s]'].entries[-1],1000)
+    return t_,npc(t_), solution['Time [s]'].entries
