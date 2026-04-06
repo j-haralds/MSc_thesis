82faf087fbb8d31010b13392049bbbb6a706e971
j-haralds <jonatan.haraldsson@icloud.com>
2026-04-06 23:23:46 +0200

    mer symbol

--- program/5_Multi/symbols/JH_symbol.py
+++ program/5_Multi/symbols/JH_symbol.py
@@ -5,30 +5,31 @@ from pysr import jl
 jl.seval("using Statistics")
 
 
-def setup_model(its = 300, pop = 25, selection = "best",run_id = 'all'):
+def setup_model(its = 500, pop = 25, selection = "best",run_id = None):
     template = TemplateExpressionSpec(
-        expressions=['f', 'g'],
-        variable_names= ['x1', 'x2', 'x3','x4'],   
-        combine = '-x1 / (abs(f(x2, x3,x4)) * abs(g(x2, x3,x4))) + x2 / abs(g(x2, x3,x4))'
+        expressions=['R', 'C'],
+        variable_names= ['x1', 'x2', 'x3','x4'],   #x1 = dU1, x2 = I, x3 = u, x4 = soc 
+        #combine = '-x1 / (exp(f(x2, x3,x4)) * exp(g(x2, x3,x4))) + x2 / exp(g(x2, x3,x4))'
+        combine = '(R(x2,x3,x4))^2 * (x2 - (C(x2,x3,x4))^2 * x1)'
     )
     model = PySRRegressor(
         model_selection=selection,
         niterations=its,
         binary_operators=["+", "*", '-','/'],
-        unary_operators=['sqrt','square', 'cube','exp'],
+        unary_operators=['sqrt','square', 'cube','exp','log'],
         elementwise_loss="loss(x, y) = (x - y)^2",
         populations=pop,
         verbosity=0, 
         expression_spec=template, 
         batching = True,
         maxsize = 15,
-        batch_size = 5000,
-        run_id = run_id
+        batch_size = 512,
+        run_id= run_id
     )
     return model
 
 
-def run_symbolic_regression(X, y, model = None,run_id = 'all'):
+def run_symbolic_regression(X, y, model = None,run_id = None):
     if model is None:
         model = setup_model(run_id = run_id)
     model.fit(X, y)
