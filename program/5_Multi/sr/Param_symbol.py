812e25e5c022294babfb804f9a5a5e151fa1d795
j-haralds <jonatan.haraldsson@icloud.com>
2026-05-06 11:23:42 +0200

    synk

--- program/5_Multi/sr/Param_symbol.py
+++ program/5_Multi/sr/Param_symbol.py
@@ -5,20 +5,24 @@ from pysr import jl
 jl.seval("using Statistics")
 
 
-def setup_model(its = int(2e2), pop = 30, selection = "best",run_id = None):
+def setup_model(its = int(1e3), pop = 30, selection = "accuracy",run_id = None):
     model = PySRRegressor(
         model_selection=selection,
         niterations=its,
         binary_operators=["+", "*", '-','/','^'],
-        unary_operators=['sqrt','square', 'cube','exp','log'],
+        unary_operators=['sqrt','square', 'cube','exp','log', 'sin', 'cos', 'tan', 'sinh', 'cosh', 'tanh'],
         populations=pop,
-        # nested_constraints={'sin':{'sin':0}, 'sin':{'cos':0}, 'sin':{'tan':0}, 'sin':{'log':0}, 'sin':{'exp':0},
-        #                     'cos':{'sin':0}, 'cos':{'cos':0}, 'cos':{'tan':0}, 'cos':{'log':0}, 'cos':{'exp':0},
-        #                     'tan':{'sin':0}, 'tan':{'cos':0}, 'tan':{'tan':0}, 'tan':{'log':0}, 'tan':{'exp':0},
-        #                     'log':{'sin':0}, 'log':{'cos':0}, 'log':{'tan':0},
-        #                     'exp':{'sin':0}, 'exp':{'cos':0}, 'exp':{'tan':0}
-        #                     },
-        verbosity=0, 
+        nested_constraints = {'sin':  {'sin': 0, 'cos': 0, 'tan': 0, 'log': 0, 'exp': 0},
+                          'cos':  {'sin': 0, 'cos': 0, 'tan': 0, 'log': 0, 'exp': 0},
+                          'tan':  {'sin': 0, 'cos': 0, 'tan': 0, 'log': 0, 'exp': 0},
+                          'sinh': {'sinh': 1, 'cosh': 1, 'tanh': 1},
+                          'cosh': {'sinh': 1, 'cosh': 1, 'tanh': 1},
+                          'tanh': {'sinh': 1, 'cosh': 1, 'tanh': 1},
+                          'log':  {'sin': 1, 'cos': 1, 'tan': 1},
+                          'exp':  {'sin': 1, 'cos': 1, 'tan': 1},
+                          },
+        verbosity=0,         
+        constraints={"^": (-1, 1)},
         batching = True,
         maxsize = 15,
         batch_size = 1024,
